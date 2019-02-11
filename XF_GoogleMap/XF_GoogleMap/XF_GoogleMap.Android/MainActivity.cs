5c1385401a651db67b0c759d24005e735123ab80
Aritra <adas@vsoftconsulting.com>
2019-02-11 19:06:45 +0530

    project done

---
+++ XF_GoogleMap/XF_GoogleMap/XF_GoogleMap.Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.GoogleMaps.Android;
+
+namespace XF_GoogleMap.Droid
+{
+    [Activity(Label = "XF_GoogleMap", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            var platformConfig = new PlatformConfig
+            {
+                BitmapDescriptorFactory = new AccessNativeBitmapConfig()
+            };
+
+            Xamarin.FormsGoogleMaps.Init(this, savedInstanceState, platformConfig);
+            LoadApplication(new App());
+
+            
+        }
+    }
+}
---
+++ XF_GoogleMap/XF_GoogleMap/XF_GoogleMap/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+
+namespace XF_GoogleMap
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            AddMarkers();
+        }
+
+        private void AddMarkers()
+        {
+            Position loc1 = new Position(17.430486, 78.341331);
+            Position loc2 = new Position(17.427579, 78.342017);
+            
+            Pin marker1 = new Pin()
+            {
+                Address = "Gachibowli",
+                IsVisible = true,
+                Label = "Microsoft Hyderabad",
+                Icon = BitmapDescriptorFactory.FromBundle("type1"),
+                Position = loc1,
+                Type = PinType.Place
+
+            };
+            Pin marker2 = new Pin()
+            {
+                Address = "Gachibowli",
+                IsVisible = true,
+                Label = "Wipro Hyderabad",
+                Icon = BitmapDescriptorFactory.FromBundle("type2"),
+                Position = loc2,
+                Type = PinType.Place
+
+            };
+
+            mapView.Pins.Add(marker1);
+            mapView.Pins.Add(marker2);
+
+            Position southwestBound = new Position(17.418652, 78.327941);
+            Position northeastBound = new Position(17.439288, 78.354593);
+            var bounds = new Bounds(southwestBound, northeastBound);
+            mapView.MoveToRegion(MapSpan.FromBounds(bounds));
+        }
+    }
+}
